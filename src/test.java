631ef3ab3c847f261c043f42b4bd997683447459
张占阳 <1592796454@qq.com>
2021-02-07 23:15:35 +0800

    zzy
    测试

--- src/test.java
+++ src/test.java
@@ -1,2 +1,10 @@
 public class test {
+<<<<<<< HEAD
+=======
+    public static void main(String[] args) {
+
+        System.out.println(1243123);
+        System.out.println();
+    }
+>>>>>>> parent of 102dd9e... 测试
 }
